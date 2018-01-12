bc67db2a404510af4b638b9f9a687d86f4586d2c
msrbharath <msrbharathraj@gmail.com>
2018-01-12 18:15:24 +0530

    Committed the UI changes into server

---
+++ code/server/dist/scripts/controllers/catalogue.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:CatalogueCtrl
+ * @description
+ * # CatalogueCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('CatalogueCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ code/server/dist/scripts/controllers/products.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:ProductsCtrl
+ * @description
+ * # ProductsCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('ProductsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
